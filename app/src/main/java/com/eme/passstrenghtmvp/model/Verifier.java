32537078c397dce57c416216314a4d20751592a7
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-03-09 16:40:13 -0300

    tests el verificador

--- app/src/main/java/com/eme/passstrenghtmvp/model/Verifier.java
+++ app/src/main/java/com/eme/passstrenghtmvp/model/Verifier.java
@@ -3,7 +3,21 @@ package com.eme.passstrenghtmvp.model;
 public class Verifier implements IVerifier {
 
     public int verificar(String password) {
-        return 0;
+        boolean largo = evaluateLenght(password);
+        boolean mayus = evaluateUpper(password);
+        boolean numero = evaluateHasNumber(password);
+
+        if (!largo) {
+            return 0;
+        } else if (largo && !mayus) {
+            return 1;
+        } else if (largo && mayus && !numero) {
+            return 2;
+        } else if (largo && mayus && numero) {
+            return 3;
+        } else {
+            return 0;
+        }
     }
 
 
@@ -14,21 +28,21 @@ public class Verifier implements IVerifier {
 
     @Override
     public boolean evaluateUpper(String password) {
-        return password.toLowerCase().equals(password);
+        return !password.toLowerCase().equals(password);
     }
 
     @Override
     public boolean evaluateHasNumber(String password) {
 
-        return password.contains("0")||
-                password.contains("1")||
-                        password.contains("2")||
-                        password.contains("3")||
-                        password.contains("4")||
-                        password.contains("5")||
-                        password.contains("6")||
-                        password.contains("7")||
-                        password.contains("8")||
-                        password.contains("9");
+        return password.contains("0") ||
+                password.contains("1") ||
+                password.contains("2") ||
+                password.contains("3") ||
+                password.contains("4") ||
+                password.contains("5") ||
+                password.contains("6") ||
+                password.contains("7") ||
+                password.contains("8") ||
+                password.contains("9");
     }
 }
--- app/src/test/java/com/eme/passstrenghtmvp/model/VerifierTest.java
+++ app/src/test/java/com/eme/passstrenghtmvp/model/VerifierTest.java
@@ -1,6 +1,5 @@
 package com.eme.passstrenghtmvp.model;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,26 +8,86 @@ import static com.google.common.truth.Truth.assertThat;
 public class VerifierTest {
 
     private Verifier verifier;
+
     @Before
     public void setUp() throws Exception {
         verifier = new Verifier();
     }
 
     @Test
-    public void verificar() {
+    public void verificar_weak() {
+        // Given
+        String password = "pass";
+
+        // When
+        int verificar = verifier.verificar(password);
+
+        // Then
+        assertThat(verificar).isEqualTo(0);
+    }
+
+    @Test
+    public void verificar_medium() {
+        // Given
+        String password = "password";
+
+        // When
+        int verificar = verifier.verificar(password);
+
+        // Then
+        assertThat(verificar).isEqualTo(1);
+    }
+
+
+    @Test
+    public void verificar_strong() {
+        // Given
+        String password = "Password";
+
+        // When
+        int verificar = verifier.verificar(password);
+
+        // Then
+        assertThat(verificar).isEqualTo(2);
+    }
+
+
+    @Test
+    public void verificar_veryStrong() {
+        // Given
+        String password = "123Password";
+
+        // When
+        int verificar = verifier.verificar(password);
+
+        // Then
+        assertThat(verificar).isEqualTo(3);
+    }
+
+    @Test
+    public void verificar_veryStrong9() {
+        // Given
+        String password = "1Poi";
+
+        // When
+        int verificar = verifier.verificar(password);
+
+        // Then
+        assertThat(verificar).isEqualTo(0);
     }
 
     @Test
     public void evaluateLenght() {
         // Given
-    String password = "password";
+        String password = "password";
 
         // When
-    boolean result = verifier.evaluateLenght(password);
+        boolean result = verifier.evaluateLenght(password);
 
         // Then
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateLength_fail() {
         // Given
@@ -43,15 +102,36 @@ public class VerifierTest {
 
 
     @Test
-    public void evaluateUpper() {
+    public void evaluateUpper_happyCase() {
+        // Given
+        String password = "Password";
+
+        // When
+        boolean result = verifier.evaluateUpper(password);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void evaluateUpper_fail() {
+        // Given
+        String password = "password";
+
+        // When
+        boolean result = verifier.evaluateUpper(password);
+
+        // Then
+        assertThat(result).isFalse();
     }
 
     @Test
     public void evaluateHasNumber_happyCase() {
         String password = "pass";
         boolean result = verifier.evaluateHasNumber(password);
-         assertThat(result).isFalse();
+        assertThat(result).isFalse();
     }
+
     @Test
     public void evaluateHasNumber_has0() {
         // Given
@@ -63,54 +143,63 @@ public class VerifierTest {
         // Then
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has1() {
         String password = "pass12";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has2() {
         String password = "pass12";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has3() {
         String password = "pass13";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has4() {
         String password = "pass4";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has5() {
         String password = "pass5";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has6() {
         String password = "pass6";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has7() {
         String password = "pass7";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has8() {
         String password = "pass8";
         boolean result = verifier.evaluateHasNumber(password);
         assertThat(result).isTrue();
     }
+
     @Test
     public void evaluateHasNumber_has9() {
         String password = "pass9";
